b85818435ff87d2f574d05694eaa25d536599fb2
ljourm <ljourm@gmail.com>
2023-03-21 00:27:12 +0900

    feat: lambda-apiの導入

--- src/functions/hello/handler.ts
+++ src/functions/hello/handler.ts
@@ -1,9 +1,12 @@
 import { APIGatewayEvent, Context } from 'aws-lambda';
+import createAPI from 'lambda-api';
+
+const api = createAPI();
+
+api.get('/status', async (req, res) => {
+  return { status: 'ok' };
+});
 
 exports.call = async (event: APIGatewayEvent, context: Context) => {
-  return {
-    statusCode: 200,
-    headers: {},
-    body: JSON.stringify({ message: "hello world" }),
-  };
+  return await api.run(event, context);
 };
