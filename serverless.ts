ec1dd21bdfefee4b4d1aa5b4f0b4223bf4e424a8
ljourm <ljourm@gmail.com>
2023-03-22 00:24:39 +0900

    fix: prettierの一斉適用

--- serverless.ts
+++ serverless.ts
@@ -1,31 +1,31 @@
-import type { AWS } from '@serverless/typescript';
+import type { AWS } from "@serverless/typescript";
 
 const serverlessConfiguration: AWS = {
-  service: 'sample-lambda-api-ts-serverless',
-  frameworkVersion: '3',
-  plugins: ['serverless-esbuild', 'serverless-offline'],
+  service: "sample-lambda-api-ts-serverless",
+  frameworkVersion: "3",
+  plugins: ["serverless-esbuild", "serverless-offline"],
   provider: {
-    name: 'aws',
-    runtime: 'nodejs18.x',
+    name: "aws",
+    runtime: "nodejs18.x",
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
     environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+      NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
     },
   },
   // import the function via paths
   functions: {
-    hello:{
+    hello: {
       handler: "src/functions/hello/handler.call",
       events: [
         {
           httpApi: {
-            method: '*',
-            path: '*',
-          }
+            method: "*",
+            path: "*",
+          },
         },
       ],
     },
@@ -36,10 +36,10 @@ const serverlessConfiguration: AWS = {
       bundle: true,
       minify: false,
       sourcemap: true,
-      exclude: ['aws-sdk'],
-      target: 'node18',
-      define: { 'require.resolve': undefined },
-      platform: 'node',
+      exclude: ["aws-sdk"],
+      target: "node18",
+      define: { "require.resolve": undefined },
+      platform: "node",
       concurrency: 10,
       // NOTE: esbuild 0.17系にあげるとエラーになる
       // https://github.com/floydspace/serverless-esbuild/pull/440
--- src/functions/hello/handler.ts
+++ src/functions/hello/handler.ts
@@ -1,13 +1,13 @@
-import { APIGatewayEvent, Context } from 'aws-lambda';
-import createAPI, { Request, Response } from 'lambda-api';
+import { APIGatewayEvent, Context } from "aws-lambda";
+import createAPI, { Request, Response } from "lambda-api";
 
 const api = createAPI({ logger: true });
 
-api.get('/status', async (req: Request, res: Response) => {
-  return { status: 'ok' };
+api.get("/status", async (req: Request, res: Response) => {
+  return { status: "ok" };
 });
 
-api.routes(true)
+api.routes(true);
 
 exports.call = async (event: APIGatewayEvent, context: Context) => {
   return await api.run(event, context);
