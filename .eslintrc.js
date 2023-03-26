8d279d36f7eecd01405b3b891e30128a488938ba
ljourm <ljourm@gmail.com>
2023-03-26 09:44:32 +0000

    chore: eslintにimport/orderを追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,26 @@ module.exports = {
   env: {
     es2021: true,
   },
-  extends: ["standard-with-typescript", "prettier"],
+  extends: ["standard-with-typescript", "plugin:import/recommended", "plugin:import/typescript", "prettier"],
   overrides: [],
   parserOptions: {
     project: "./tsconfig.json",
   },
+  settings: {
+    "import/resolver": {
+      typescript: true,
+      node: true,
+    },
+  },
   rules: {
     "@typescript-eslint/prefer-nullish-coalescing": "off",
     "@typescript-eslint/strict-boolean-expressions": "off",
+    "import/order": [
+      "error",
+      {
+        alphabetize: { order: "asc" },
+        warnOnUnassignedImports: true,
+      },
+    ],
   },
 };
